4e5558021ea9acf33c32a0dd3ca876e3d178c18d
Marilou Ferrater <marilouferrater24@gmail.com>
2025-11-06 18:49:42 +0800

    feature(reservations): Update Restore Feature

--- rr_project/static/reservations/js/reservation_list.js
+++ rr_project/static/reservations/js/reservation_list.js
@@ -1,3 +1,4 @@
+// Prompt user for a cancellation reason
 async function showCancelReason(form) {
   window.MessageBox.showPrompt("Please provide a reason for cancellation:", {
     title: "Cancel Reservation",
@@ -7,7 +8,7 @@ async function showCancelReason(form) {
         return "Please enter a reason before submitting.";
       }
       return true;
-    },
+    }
   })
   .then(reason => {
     submitCancel(reason, form);
@@ -17,6 +18,7 @@ async function showCancelReason(form) {
   });
 }
 
+// Submit cancel form with reason
 function submitCancel(reason, form) {
   if (form) {
     form.querySelector(".cancel-reason-input").value = reason;
@@ -24,22 +26,16 @@ function submitCancel(reason, form) {
   }
 }
 
-function deleteReservation(form) {
-  if (form) {
-    window.MessageBox.showConfirm("Are you sure you want to move this reservation to trash?", () => {
-      form.submit();
-    });
-  }
-}
-
+// Confirm before restoring cancelled reservation
 function restoreReservation(form) {
   if (form) {
-    window.MessageBox.showConfirm("Restore this reservation?", () => {
+    window.MessageBox.showConfirm("Do you want to restore this cancelled reservation?", () => {
       form.submit();
     });
   }
 }
 
+// Redirect to edit reservation page
 function editReservation(reservationId) {
   window.location.href = `/reservations/${reservationId}/edit/`;
 }
