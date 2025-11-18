c207703808d14b6f701fdcf3d691109833770582
joanaxyz <joanacarlagako15@gmail.com>
2025-11-19 04:41:27 +0800

    update

--- rr_project/rr_project/settings.py
+++ rr_project/rr_project/settings.py
@@ -159,6 +159,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+WHITENOISE_MIMETYPES = {
+    '.js': 'application/javascript',
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
